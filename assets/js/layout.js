619c1692c6f43d4988076aafc24d9030d2782448
nightelf973 <71837805+nightelf973@users.noreply.github.com>
2025-06-17 19:48:31 +0100

    Create layout.js

---
+++ assets/js/layout.js
@@ -0,0 +1,36 @@
+
+fetch('/forming-allies/includes/navbar.html')
+    .then(response => response.text())
+    .then(html => {
+      document.body.insertAdjacentHTML('afterbegin', html);
+    })
+    .then(() => {
+      var activeInput = document.getElementById('active-nav-title').value;
+      document.getElementById(activeInput).classList.add('active');  
+      if (localStorage.getItem("login") === "true") {
+        const discussion = document.getElementById('discussion-link');
+        const login = document.getElementById('login-logout-section');
+        discussion.innerHTML = `
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Discussion</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="/forming-allies/Discussion">Latest Posts</a>
+            <a class="dropdown-item" href="/forming-allies/Discussion/myposts.html">My Posts</a>
+            <a class="dropdown-item" href="/forming-allies/Discussion/createpost.html">Create Post</a>
+          </div>
+        </li>`;
+        login.innerHTML = `<li><a class='nav-link active' href='/forming-allies/Account/logout.html'>Logout</a></li>`;
+      }
+    })
+    .catch(error => {
+      console.error('Error loading navbar', error);
+    });
+  fetch('/forming-allies/includes/footer.html')
+    .then(response => response.text())
+    .then(html => {
+      document.body.insertAdjacentHTML('beforeend', html);
+    })
+    .catch(error => {
+      console.error('Error loading footer', error);
+    });
+
