c756c04ecb06cabd054904326eced73fdcbf4f23
nightelf973 <71837805+nightelf973@users.noreply.github.com>
2025-06-17 20:26:24 +0100

    Update layout.js

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -12,14 +12,14 @@ fetch('/forming-allies/includes/navbar.html')
         const login = document.getElementById('login-logout-section');
         discussion.innerHTML = `
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Discussion</a>
+          <a id="discussion-nav" class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Discussion</a>
           <div class="dropdown-menu">
             <a class="dropdown-item" href="/forming-allies/Discussion">Latest Posts</a>
             <a class="dropdown-item" href="/forming-allies/Discussion/myposts.html">My Posts</a>
             <a class="dropdown-item" href="/forming-allies/Discussion/createpost.html">Create Post</a>
           </div>
         </li>`;
-        login.innerHTML = `<li><a class='nav-link active' href='/forming-allies/Account/logout.html'>Logout</a></li>`;
+        login.innerHTML = `<li><a id="account-nav" class='nav-link active' href='/forming-allies/Account/logout.html'>Logout</a></li>`;
       }
     })
     .catch(error => {
